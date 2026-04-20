d262219c87bc7e56e3b324bcb5a077bdf9506815
LuisZumbado99 <126292576+LuisZumbado99@users.noreply.github.com>
2026-04-20 14:04:55 -0600

    Update top.sv
    Modificaciones necesarias para implementar el módulo de LEDs

--- Laboratorio_2/Modulos_de_trabajo/top.sv
+++ Laboratorio_2/Modulos_de_trabajo/top.sv
@@ -1,31 +1,35 @@
 module top (
     input  logic CLK100MHZ,
     input  logic rst_btn,
+    input  logic [15:0] sw,
     output logic [15:0] led
 );
 
+ 
+    // Señales de reloj
+ 
     logic clk_sys;
     logic pll_locked;
 
-
+ 
     // PLL
 
-   clock_gen clk_inst (
+    clock_gen clk_inst (
         .clk_in(CLK100MHZ),
         .rst(rst_btn),
         .clk_out(clk_sys),
         .locked(pll_locked)
     );
 
-
-    // Reset activo en bajo para PicoRV32
-
-   logic resetn;
+ 
+    // Reset activo en bajo
+ 
+    logic resetn;
     assign resetn = pll_locked;
- 
- 
-    // Señales de memoria
 
+ 
+    // Señales de memoria (CPU)
+ 
     logic        mem_valid;
     logic        mem_ready;
     logic [31:0] mem_addr;
@@ -33,9 +37,14 @@ module top (
     logic [3:0]  mem_wstrb;
     logic [31:0] mem_rdata;
 
+ 
+    // Señal interna de LEDs
+ 
+    logic [15:0] led_internal;
 
+ 
     // CPU PicoRV32
-
+ 
     picorv32 cpu (
         .clk         (clk_sys),
         .resetn      (resetn),
@@ -46,12 +55,11 @@ module top (
         .mem_wdata   (mem_wdata),
         .mem_wstrb   (mem_wstrb),
         .mem_rdata   (mem_rdata)
-
     );
 
-
-    // Memoria (ROM + RAM)
-
+ 
+    // Memoria + periféricos
+ 
     memory mem_inst (
         .clk        (clk_sys),
         .mem_valid  (mem_valid),
@@ -59,11 +67,14 @@ module top (
         .mem_addr   (mem_addr),
         .mem_wdata  (mem_wdata),
         .mem_wstrb  (mem_wstrb),
-        .mem_rdata  (mem_rdata)
+        .mem_rdata  (mem_rdata),
+        .led_out    (led_internal),
+        .sw_in      (sw)
     );
 
-    // Debug (opcional)
-
-    assign led[0] = pll_locked;
+ 
+    // Salida a LEDs físicos
+ 
+    assign led = led_internal;
 
 endmodule
