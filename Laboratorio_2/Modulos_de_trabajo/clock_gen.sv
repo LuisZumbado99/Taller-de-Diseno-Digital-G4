dda418734632077229b74d80d03e0e655c50b682
LuisZumbado99 <126292576+LuisZumbado99@users.noreply.github.com>
2026-04-28 21:16:28 -0600

    Update clock_gen.sv
    Correcciones finales

--- Laboratorio_2/Modulos_de_trabajo/clock_gen.sv
+++ Laboratorio_2/Modulos_de_trabajo/clock_gen.sv
@@ -1,21 +1,16 @@
 module clock_gen (
-    input  logic clk_in,     // 100 MHz (Nexys4)
-    input  logic rst,        // reset externo
-    output logic clk_out,    // reloj generado
-    output logic locked      // indica estabilidad
+    input  logic clk_in,
+    input  logic rst,
+    output logic clk_out,
+    output logic locked
 );
 
-    // Señal interna de reset para el PLL
-    logic resetn;
+    wire pll_reset = ~rst; 
 
-    assign resetn = ~rst;
-
-    // Instancia del Clock Wizard (PLL/MMCM)
     clk_wiz_0 pll_inst (
         .clk_in1(clk_in),
-        .reset(rst),
+        .reset(pll_reset),
         .clk_out1(clk_out),
         .locked(locked)
     );
-
 endmodule
